8ad49fd058672615350042c51918166305006ce6
Manish Rawat <manish_rawat@comcast.com>
2020-09-12 17:26:12 -0400

    checking if cheerio supported

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+const queryString = require('query-string');
+import got from "got";
+import cheerio from "cheerio";
+// const nurlresolver = require('nurlresolver');
+
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-  const requestUrl = request.url
-  return new Response(`Hello cloud flare worker! You requested ${requestUrl}`, {
-    headers: { "content-type": "text/plain" }
+  console.log('handling request...');
+  const { query } = queryString.parseUrl(request.url);
+  let result = {};
+  if (query.q) {
+    const { q, m } = query;
+    // result = await nurlresolver.resolve(q, {
+    //   extractMetaInformation: m && m === 'true'
+    // });
+
+    const response = await got(q);
+    const $ = cheerio.load(response.body);
+    const title = $('title').first()
+    result = title;
+    //this is just for test purposes
+  } else {
+    result = { error: 'Query param q not defined' }
+  }
+  return new Response(result, {
+    headers: { "content-type": "application/json" }
   })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
+const webpack = require('webpack')
+
 module.exports = {
-    target: "webworker",
-    entry: "./index.js",
-    mode: "production"
-  }
+  mode: "production",    
+  context: __dirname,
+  entry: "./index.js",
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "global.GENTLY": false
+    }),
+    // new webpack.LoaderOptionsPlugin({
+    //   // test: /\.xxx$/, // may apply this only for some modules
+    //   options: {
+    //     alias: {
+    //       'inherits': 'inherits/inherits_browser.js',
+    //       'superagent': 'superagent/lib/client',
+    //       'emitter': 'component-emitter',
+    //     },
+    //   }
+    // })
+  ],
+}
